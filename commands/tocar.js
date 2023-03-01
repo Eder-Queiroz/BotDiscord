11404380ccf7b4c68f3e9d9ce0112e2c26bb5c9e
Eder Queiroz <62737156+Eder-Queiroz@users.noreply.github.com>
2023-03-01 11:27:30 -0300

    create play song for youtube

--- commands/tocar.js
+++ commands/tocar.js
@@ -1,11 +1,11 @@
-const {SlashCommandBuilder, MessageEmbed} = require("discord.js");
+const {SlashCommandBuilder, EmbedBuilder} = require("discord.js");
 const { QueryType } = require("discord-player")
 
 module.exports = {
 
     data: new SlashCommandBuilder()
-        .setName("play")
-        .setDescription("play a song from YouTube.")
+        .setName("tocar")
+        .setDescription("Toco uma para vc ;).")
         .addSubcommand(subcommand =>
             subcommand
                 .setName("search")
@@ -22,9 +22,9 @@ module.exports = {
         )
         .addSubcommand(subcommand =>
             subcommand
-                .setName("song")
-                .setDescription("Plays a single song from YT")
-                .addStringOption(option => option.setName("url").setDescription("the song's url").setRequired(true))
+                .setName("musiquinha")
+                .setDescription("Canto um musiquinha do youtube. :)")
+                .addStringOption(option => option.setName("url").setDescription("Coloca a url em mim!").setRequired(true))
         ),
     
         async execute(interection, client) {
@@ -38,6 +38,59 @@ module.exports = {
             if(!queue.connection) {
                 await queue.connect(interection.member.voice.channel);
             }
+
+            let embed = new EmbedBuilder();
+
+            if(interection.options.getSubcommand() === "musiquinha") {
+
+                let url = interection.options.getString("url");
+                
+                // busca da url usando discord-player
+                const result = await client.player.search(url, {
+                    requestedBy: interection.user,
+                    searchEngine: QueryType.YOUTUBE_VIDEO
+                });
+
+                // caso não encontre nada na busca, retornar uma mensagem ao usuario.
+                if(result.tracks.length === 0) {
+                    return interection.reply("Sem musiquinha. Você pesquisou errado, burrão!");
+                }
+
+                // adicionando track na fila
+                const song = result.tracks[0];
+
+                await queue.addTrack(song);
+                embed.setDescription(`**[${song.title}](${song.url})** foi adicionada a fila`)
+                .setThumbnail(song.thumbnail)
+                .setFooter({text: `Duration: ${song.duration}`})
+
+            } else if(interection.options.getSubcommand() === "playlist") {
+                
+                // busca pela playlist usando o discord-player
+                let url = interection.options.getString("url");
+                const result = await client.player.search(url, {
+                    requestedBy: interection.user,
+                    searchEngine: QueryType.YOUTUBE_PLAYLIST
+                });
+
+                if(result.tracks.length === 0) {
+                    return interection.reply("Sem playlist. É uma anta!\n Ainda consegue pesquisar errado.");
+                }
+
+                const playlist = result.playlist;
+
+                await queue.addTracks(result.tracks);
+
+                // embed
+                // .setDescription(`**${result.tracks.length} songs from [${playlist.title}](${playlist.url})** foi adicionada a fila`)
+                // .setThumbnail(playlist.thumbnail);
+            }
+
+            if(!queue.playing) {
+                await queue.play();
+            }
+
+            await interection.reply({embeds: [embed]});
 
         }
 
